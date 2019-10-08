5cc5e58e57e319e70881383376b5dfede05be514
Paweł Pacana <pawel.pacana@gmail.com>
2019-10-08 15:51:04 +0200

    Adapt mappers to timestamp passed in SerializedRecord.

--- ruby_event_store/spec/mappers/json_mapper_spec.rb
+++ ruby_event_store/spec/mappers/json_mapper_spec.rb
@@ -10,15 +10,19 @@ module RubyEventStore
     RSpec.describe JSONMapper do
       it_behaves_like :mapper, described_class.new, TimestampEnrichment.with_timestamp(SomethingHappenedJSON.new)
 
+      let(:time) { Time.now.utc }
+      let(:serialized_time) { time.iso8601(TIME_PRECISION) }
       let(:data) { { some_attribute: 5 } }
       let(:metadata) { { some_meta: 1 } }
       let(:event_id) { SecureRandom.uuid }
       let(:serialized_record_event_type) { SomethingHappenedJSON.name }
       let(:domain_event) do
-        SomethingHappenedJSON.new(
-          event_id: event_id,
-          data:     data,
-          metadata: metadata,
+        TimestampEnrichment.with_timestamp(
+          SomethingHappenedJSON.new(
+            event_id: event_id,
+            data:     data,
+            metadata: metadata,
+          )
         )
       end
       let(:serialized_record) do
@@ -27,6 +31,7 @@ module RubyEventStore
           data:       data,
           metadata:   metadata,
           event_type: serialized_record_event_type,
+          timestamp:  serialized_time,
         )
       end
 
@@ -52,14 +57,14 @@ module RubyEventStore
         it { is_expected.to have_attributes(event_id: domain_event.event_id) }
         it { is_expected.to have_attributes(data: domain_event.data) }
         it 'includes the expected metadata' do
-          expect(serialized_record_to_event.metadata.to_h).to eq(metadata)
+          expect(serialized_record_to_event.metadata.to_h).to eq(metadata.merge(timestamp: time))
         end
 
         context 'when metadata has strings for keys' do
           let(:metadata) { { 'some_meta' => 1 } }
 
           it 'includes metadata with symbolized keys' do
-            expect(serialized_record_to_event.metadata.to_h).to eq(some_meta: 1)
+            expect(serialized_record_to_event.metadata.to_h).to eq(some_meta: 1, timestamp: time)
           end
         end
       end
